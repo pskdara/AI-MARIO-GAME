6482dedce5953cc63c64a5200af8f422aba183ae
pskdara <73217595+pskdara@users.noreply.github.com>
2021-12-23 00:19:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload() {
+	mario_gameover = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
+	instializeInSetup(mario);
+	
+	
+
+}
+
+
+   
+function draw() {
+	game();
+}
+
+
+
+
+
+
